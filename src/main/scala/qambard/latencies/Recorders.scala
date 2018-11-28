025cbba643713bd30af4898fc3629b8237734140
quentin <quentin.ambard@datastax.com>
2018-11-28 16:01:58 +0100

    add jar

---
+++ src/main/scala/qambard/latencies/Recorders.scala
@@ -0,0 +1,7 @@
+package qambard.latencies
+
+import org.HdrHistogram.Histogram
+import qambard.latencies.LatenciesTest.{highestTrackableValue, numberOfSignificantValueDigits}
+
+object Recorders {
+}
